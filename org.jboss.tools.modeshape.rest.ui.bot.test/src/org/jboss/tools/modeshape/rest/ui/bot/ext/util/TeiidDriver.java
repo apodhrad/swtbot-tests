22129e51c97e9e1429398a32435d6309c12df7df
Andrej Podhradsky <apodhrad@redhat.com>
2013-02-01 17:03:33 +0100

    Fixed getParentLogger

--- org.jboss.tools.modeshape.rest.ui.bot.test/src/org/jboss/tools/modeshape/rest/ui/bot/ext/util/TeiidDriver.java
+++ org.jboss.tools.modeshape.rest.ui.bot.test/src/org/jboss/tools/modeshape/rest/ui/bot/ext/util/TeiidDriver.java
@@ -66,6 +66,6 @@ public class TeiidDriver implements Driver {
 
 	@Override
 	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-		return Logger.getLogger("TeiidDriver");
+		return this.driver.getParentLogger();
 	}
 }
