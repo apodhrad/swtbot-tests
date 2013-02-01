7decbb0a2500d377140682ac009810eef4b9b1bd
Andrej Podhradsky <apodhrad@redhat.com>
2013-02-01 09:15:19 +0100

    Allow specyfying test suite

--- org.jboss.tools.esb.ui.bot.test/src/org/jboss/tools/esb/ui/bot/tests/ESBAllBotTests.java
+++ org.jboss.tools.esb.ui.bot.test/src/org/jboss/tools/esb/ui/bot/tests/ESBAllBotTests.java
@@ -10,18 +10,6 @@
   ******************************************************************************/
 package org.jboss.tools.esb.ui.bot.tests;
  
-import org.jboss.tools.esb.ui.bot.tests.examples.HelloWorld;
-import org.jboss.tools.esb.ui.bot.tests.examples.HelloWorldAction;
-import org.jboss.tools.esb.ui.bot.tests.examples.HelloWorldFileAction;
-import org.jboss.tools.esb.ui.bot.tests.examples.SmooksCSV2XML;
-import org.jboss.tools.esb.ui.bot.tests.examples.SmooksXML2POJO;
-import org.jboss.tools.esb.ui.bot.tests.examples.SmooksXML2XMLSimple;
-import org.jboss.tools.esb.ui.bot.tests.examples.SmooksXML2XMLDateManipulation;
-import org.jboss.tools.esb.ui.bot.tests.examples.SimpleEAPTest;
-import org.jboss.tools.esb.ui.bot.tests.examples.WebServiceConsumer1;
-import org.jboss.tools.esb.ui.bot.tests.examples.WebServiceProducer;
-import org.jboss.tools.esb.ui.bot.tests.examples.WebServiceProducerHttp;
-import org.jboss.tools.esb.ui.bot.tests.examples.WebServiceProducerSocket;
 import org.jboss.tools.ui.bot.ext.RequirementAwareSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite.SuiteClasses;
@@ -31,19 +19,7 @@ import org.junit.runners.Suite.SuiteClasses;
 	CreateRuntimeFromSOA.class,
 	NewProjectUsingRuntime.class,
 	NewProjectUsingBundledInEAP.class,
-	Editing.class,
-	HelloWorld.class,
-	HelloWorldAction.class,
-	HelloWorldFileAction.class,
-	SmooksCSV2XML.class,
-	SmooksXML2POJO.class,
-	SmooksXML2XMLDateManipulation.class,
-	SmooksXML2XMLSimple.class,
-	WebServiceConsumer1.class,
-	WebServiceProducer.class,  // http://lists.jboss.org/pipermail/jbosstools-dev/2008-December/002559.html
-	WebServiceProducerHttp.class,
-	WebServiceProducerSocket.class
-	//SimpleEAPTest.class
+	Editing.class
 	})
 @RunWith(RequirementAwareSuite.class)
 public class ESBAllBotTests {
